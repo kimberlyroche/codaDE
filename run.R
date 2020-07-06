28a14f750b142e2abae5f170a77c0d3111f1248c
Kim Roche <kimberly.e.roche@gmail.com>
2020-07-06 11:30:21 -0400

    Updates to wrapper script for mean abundance filtering

--- run.R
+++ run.R
@@ -5,20 +5,31 @@ library(codaDE)
 
 args <- commandArgs(trailing = TRUE)
 if(length(args) < 2) {
-  stop("Missing arguments: {number of genes} {number of samples per treatment}")
+  stop("Missing arguments: {number of genes} {number of samples per treatment} {run label}")
 }
 p <- as.numeric(args[1])
 n <- as.numeric(args[2])
-rarefy <- FALSE
-if(length(args) > 2) {
-  rarefy <- as.logical(args[3])
-}
+run_label <- args[3]
+# optional arguments
 use_ALR <- FALSE
 if(length(args) > 3) {
   use_ALR <- as.logical(args[4])
 }
+filter_abundance <- 0
+if(length(args) > 4) {
+  filter_abundance <- as.numeric(args[5])
+}
+rarefy <- FALSE
+if(length(args) > 5) {
+  rarefy <- as.logical(args[6])
+}
 
-run_label <- "RNAseq_like_CODA"
-
-sweep_RNAseq(p, n, run_label = run_label, de_sweep = seq(from = 0.1, to = 0.9, by = 0.1), corr_sweep = seq(from = 0.1, to = 0.9, by = 0.1),
-             output_file = "results.txt", alpha = 0.05, use_ALR = use_ALR, rarefy = rarefy)
+sweep_RNAseq(p,
+             n,
+             run_label = run_label,
+             de_sweep = c((1/10), (1/4), (2/3)),
+             corr_sweep = c(0, 0.9),
+             output_file = "results_RNAseq.tsv",
+             use_ALR = use_ALR,
+             filter_abundance = filter_abundance,
+             rarefy = rarefy)
