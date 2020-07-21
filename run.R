ac912a2b70c9d780164f6a607c8c53a4dc98718e
Kim Roche <kimberly.e.roche@gmail.com>
2020-07-21 18:22:30 -0400

    Added single-cell simulation

--- run.R
+++ run.R
@@ -1,40 +1,37 @@
-# This code runs and evaluates error in differential expression calls for a single simulation instance with
-# the desired characteristics.
+# This code runs and evaluates error in differential expression calls for a single
+# simulation instance with the desired characteristics.
 
 library(codaDE)
+library(optparse)
 
-args <- commandArgs(trailing = TRUE)
-if(length(args) < 2) {
-  stop("Missing arguments: {number of genes} {number of samples per treatment} {run label} {opt: single-cell dataset label}")
-}
-p <- as.numeric(args[1])
-n <- as.numeric(args[2])
-run_label <- args[3]
-# optional arguments
-sc_dataset_name <- NULL
-if(length(args) > 3) {
-  sc_dataset_name <- args[4]
-}
-use_ALR <- FALSE
-if(length(args) > 4) {
-  use_ALR <- as.logical(args[5])
-}
-filter_abundance <- 0
-if(length(args) > 5) {
-  filter_abundance <- as.numeric(args[6])
-}
-rarefy <- FALSE
-if(length(args) > 6) {
-  rarefy <- as.logical(args[7])
-}
+option_list = list(
+  make_option(c("--p"), type = "numeric", default = NULL, 
+              help = "number of genes", metavar = "numeric"),
+  make_option(c("--n"), type = "numeric", default = NULL, 
+              help = "number of samples per condition", metavar = "numeric"),
+  make_option(c("--k"), type = "numeric", default = NULL, 
+              help = "number of cell types (if simulating single-cell)", metavar = "numeric"),
+  make_option(c("--run_label"), type = "character", default = NULL, 
+              help = "label associated with this simulation (to appear in output file)", metavar = "character"),
+  make_option(c("--use_ALR"), type = "logical", default = FALSE, 
+              help = "flag indicating whether or not to perform differential abundance testing on logratios", metavar = "logical"),
+  make_option(c("--filter_abundance"), type = "numeric", default = 0, 
+              help = "minimum average abundance of a gene on which to test differential abundance", metavar = "numeric"),
+  make_option(c("--rarefy"), type = "logical", default = FALSE, 
+              help = "flag indicating whether or not to rarefy samples", metavar = "logical")
+); 
 
-sweep_RNAseq(p,
-             n,
-             run_label = run_label,
-             sc_dataset_name = sc_dataset_name,
-             de_sweep = c((1/10)),
-             corr_sweep = c(0),
-             output_file = paste0("results_",run_label,".tsv"),
-             use_ALR = use_ALR,
-             filter_abundance = filter_abundance,
-             rarefy = rarefy)
+opt_parser = OptionParser(option_list=option_list);
+opt = parse_args(opt_parser);
+
+sweep_simulations(p = opt$p,
+                  n = opt$n,
+                  run_label = opt$run_label,
+                  k = opt$k,
+                  de_sweep = c(0.1, 0.5, 0.9),
+                  corr_sweep = c(0.1, 0.5, 0.9)
+                  output_file = "results.txt",
+                  alpha = 0.05,
+                  use_ALR = opt$use_ALR,
+                  filter_abundance = opt$filter_abundance,
+                  rarefy = opt$rarefy)
