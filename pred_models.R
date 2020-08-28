ddf185e60d857a497f1c2abd353589731f686ffd
Kimberly Roche <ker48@c1-01-3.genome.duke.edu>
2020-08-27 20:37:11 -0400

    HARDAC changes, predictive model script consolidation

---
+++ pred_models.R
@@ -0,0 +1,84 @@
+# This is a first pass at a predictive model.
+
+library(ggplot2)
+library(gridExtra)
+library(betareg)
+library(kernlab)
+
+fit_GP <- T
+
+# (1) Parse the simulated data.
+
+output_file <- file.path("simulated_data/results_nofilter.tsv")
+
+eval_data <- read.table(output_file, header = T, stringsAsFactors = F)
+# Note: The latest run includes an insertion into the 6th column of a logical flag that indicates
+#       whether or not the NB LRT was used for DE (as opposed to the log-LM + permutations).
+#       These labels will have to be updated if using those results.
+
+# (2) Fit and evaluate a Beta regression model.
+# Note: We need to add a tiny value because betareg only allows outcomes (0,1).
+
+addend <- 0.00001
+eval_data$fpr <- eval_data$fp / (eval_data$fp + eval_data$tn) + addend
+eval_data$fnr <- eval_data$fn / (eval_data$fn + eval_data$tp) + addend
+
+fit_fpr <- betareg(fpr ~ p + proportion_da + size_factor_correlation,
+                   data = eval_data, link = "log")
+predicted_fpr <- predict(fit_fpr, eval_data)
+
+fit_fnr <- betareg(fnr ~ p + proportion_da + size_factor_correlation,
+                   data = eval_data, link = "log")
+predicted_fnr <- predict(fit_fnr, eval_data)
+
+plot_df <- data.frame(predicted_values = predicted_fpr,
+                      true_values = eval_data$fpr,
+                      p = as.factor(eval_data$p),
+                      proportion_da = as.factor(eval_data$proportion_da),
+                      size_factor_correlation = as.factor(eval_data$size_factor_correlation),
+                      which = "fpr")
+plot_df <- rbind(plot_df,
+                 data.frame(predicted_values = predicted_fnr,
+                 true_values = eval_data$fnr,
+                 p = as.factor(eval_data$p),
+                 proportion_da = as.factor(eval_data$proportion_da),
+                 size_factor_correlation = as.factor(eval_data$size_factor_correlation),
+                 which = "fnr"))
+plot_df$which <- as.factor(plot_df$which)
+
+p <- ggplot(plot_df) +
+     geom_point(aes(x = true_values, y = predicted_values)) +
+     facet_wrap(vars(which), nrow = 1, scales = "free")
+ggsave("betareg_model.png", p, units = "in", dpi = 150, height = 5, width = 10)
+
+# (3) Fit and evaluate a GP regression model.
+
+if(fit_GP) {
+  # x should be N x D
+  # y should be N x 1
+
+  # should probably scale these?
+  x <- cbind(eval_data$p, eval_data$proportion_da, eval_data$size_factor_correlation)
+  y <- eval_data$fpr
+
+  dim(y) <- c(length(y), 1)
+  fit_fpr <- gausspr(x, y, scaled = TRUE)
+  predicted_fpr <- predict(fit_fpr, x)
+
+  y <- eval_data$fnr
+  dim(y) <- c(length(y), 1)
+  fit_fnr <- gausspr(x, y, scaled = TRUE)
+  # fit can be adjusted via var = ... and kpar = list(sigma = ...)
+  predicted_fnr <- predict(fit_fnr, x)
+
+  plot_df <- data.frame(predicted_values = predicted_fpr, true_values = eval_data$fpr, which = "fpr")
+  plot_df <- rbind(plot_df,
+                   data.frame(predicted_values = predicted_fnr, true_values = eval_data$fnr, which = "fnr"))
+  plot_df$which <- as.factor(plot_df$which)
+
+  p <- ggplot(plot_df) +
+      geom_point(aes(x = true_values, y = predicted_values)) +
+      facet_wrap(vars(which), nrow = 1, scales = "free")
+  ggsave("GPreg_model.png", p, units = "in", dpi = 150, height = 5, width = 10)
+}
+
--- pull_results.R
+++ pull_results.R
@@ -1,3 +1,6 @@
+# run on 8K simulation files, this takes about 2-3 hrs.
+# it's the loading of each individual data set that's rough (TBD)
+
 output_file <- file.path("simulated_data/metadata.tsv")
 metadata <- read.table(output_file, header = T, stringsAsFactors = F)
 metadata <- metadata[metadata$filter_threshold == 0,]
@@ -7,22 +10,58 @@ cap <- as.numeric(args[1])
 
 data_dir <- "simulated_data"
 outfile <- paste0("results_nofilter_",cap,".tsv")
+#outfile <- "results_nofilter.tsv"
+
 metadata$tp <- NULL
 metadata$fp <- NULL
 metadata$tn <- NULL
 metadata$fn <- NULL
+metadata$median_expr_da_quantile <- NULL
+metadata$net_dir_da <- NULL
+metadata$sparsity <- NULL
+metadata$sim_entropy <- NULL
 
 lower_lim <- max(1, cap - 999)
 upper_lim <- min(nrow(metadata), cap)
+#lower_lim <- 1
+#upper_lim <- nrow(metadata)
 for(idx in lower_lim:upper_lim) {
   cat(paste0("Pulling data set ",idx," / ",nrow(metadata),"\n"))
-  data <- readRDS(file.path(data_dir, metadata$filename[idx]))$result
-  metadata[idx,"tp"] <- data$tp
-  metadata[idx,"fp"] <- data$fp
-  metadata[idx,"tn"] <- data$tn
-  metadata[idx,"fn"] <- data$fn
+  cat(paste0("\t",metadata$filename[idx],"\n"))
+  data <- readRDS(file.path(data_dir, metadata$filename[idx]))
+  if("result" %in% names(data)) {
+    data$properties$p <- data$result$p
+    data$properties$proportion_da <- data$result$proportion_da
+    data$properties$size_factor_correlation <- data$result$size_factor_correlation
+    data$properties$median_expr_da_quantile <- data$result$median_expr_da_quantile
+    data$properties$net_dir_da <- data$result$net_dir_da
+    data$properties$sparsity <- data$result$sparsity
+    data$properties$sim_entropy <- data$result$sim_entropy
+    data$results <- list(data$result)
+    data$result <- NULL
+    data$results[[1]]$p <- NULL
+    data$results[[1]]$proportion_da <- NULL
+    data$results[[1]]$size_factor_correlation <- NULL
+    data$results[[1]]$median_expr_da_quantile <- NULL
+    data$results[[1]]$net_dir_da <- NULL
+    data$results[[1]]$sparsity <- NULL
+    data$results[[1]]$sim_entropy <- NULL
+    saveRDS(data, file.path(data_dir, metadata$filename[idx]))
+  }
+
+  # record the metadata we want
+  metadata[idx,"tp"] <- data$results[[1]]$tp
+  metadata[idx,"fp"] <- data$results[[1]]$fp
+  metadata[idx,"tn"] <- data$results[[1]]$tn
+  metadata[idx,"fn"] <- data$results[[1]]$fn
+  metadata[idx,"median_expr_da_quantile"] <- data$properties$median_expr_da_quantile
+  metadata[idx,"net_dir_da"] <- data$properties$net_dir_da
+  metadata[idx,"sparsity"] <- data$properties$sparsity
+  metadata[idx,"sim_entropy"] <- data$properties$sim_entropy
+  # now fix the structure of the saved object
 }
 
 metadata <- metadata[complete.cases(metadata),]
 
 write.table(metadata, file = file.path(data_dir, outfile), quote = FALSE, sep = '\t', row.names = FALSE)
+
