dcf229cfc2d289902e495b759996956df5e0db8b
Kim Roche <kimberly.e.roche@gmail.com>
2021-04-02 12:04:26 -0400

    Poster plots

--- plot_compiled.R
+++ plot_compiled.R
@@ -5,16 +5,22 @@ library(pROC)
 
 setwd("C:/Users/kimbe/Documents/codaDE")
 
-p <- 100
+# Select global palette for 5 methods
+# palette <- generate_highcontrast_palette(1000)
+# palette <- sample(palette, size = 5)
+# points <- data.frame(x = rnorm(5), y = rnorm(5), label = factor(1:5))
+# ggplot(points, aes(x = x, y = y, color = label)) +
+#   geom_point(size = 10) +
+#   scale_color_manual(values = palette)
 
-data <- readRDS(file.path("output", paste0("simresults_p",p,"_simulated_all.rds")))
-
-# Peek at realized fold changes
-# hist(data$delta_mean_v2)
+palette <- c("#46A06B", "#B95D6E", "#EF82BB", "#755A7F", "#E3C012")
 
 # ------------------------------------------------------------------------------
 #   ROC curve(ish) version
 # ------------------------------------------------------------------------------
+
+p <- 15000
+data <- readRDS(file.path("output", paste0("simresults_p",p,"_simulated_all.rds")))
 
 plot_data <- data %>%
   select(delta_mean_v2, rate, rate_type, method) %>%
@@ -23,16 +29,13 @@ plot_data <- data %>%
 plot_data$method <- factor(plot_data$method)
 levels(plot_data$method) <- c("NB GLM", "DESeq2", "MAST", "scran")
 
-if(!exists("cpalette")) {
-  cpalette <- generate_highcontrast_palette(100)
-  cpalette <- sample(cpalette, size = length(levels(plot_data$method)))
-}
-
 ggplot(plot_data, aes(x = fpr, y = tpr, color = method)) + # plot an ROC curve
   geom_point(size = 2, alpha = 0.5) +
-  scale_color_manual(values = cpalette) +
-  xlim(c(0,1)) +
-  ylim(c(0,1)) +
+  scale_color_manual(values = palette) +
+  # xlim(c(0,1)) +
+  # ylim(c(0,1)) +
+  xlim(c(0,0.75)) +
+  ylim(c(0.25,1)) +
   xlab("FPR") +
   ylab("TPR") +
   facet_wrap(. ~ method) +
@@ -43,37 +46,61 @@ ggsave(file.path("output", "images", save_filename <- paste0("DE_p",p,"_all_mode
        width = 5)
 
 # ------------------------------------------------------------------------------
-#   Partial abundance version (with NB GLM only)
+#   Partial abundance version (with NB GLM only) TBD w/ data
 # ------------------------------------------------------------------------------
 
 partial_plot <- TRUE
 
 if(partial_plot) {
-  plot_data <- data %>%
-    select(delta_mean_v2, rate, rate_type, method) %>%
+  p <- 100
+  data <- readRDS(file.path("output", paste0("simresults_p",p,"_simulated_all.rds")))
+  data <- data %>%
     filter(method %in% c("baseline", "spike_in")) %>%
-    pivot_wider(names_from = rate_type, values_from = rate)
-  plot_data$method <- factor(plot_data$method)
-  levels(plot_data$method) <- c("NB GLM", "NB GLM (partial totals)")
-
-  if(!exists("cpalette")) {
-    cpalette <- generate_highcontrast_palette(100)
-    cpalette <- sample(cpalette, size = length(levels(plot_data$method)))
+    filter(rate_type == "fpr") %>%
+    filter(delta_mean_v2 > 2)
+  
+  plot_data <- data.frame(cor_totals = c(), delta_fpr = c())
+  for(i in seq(from = 1, to = (nrow(data)-1), by = 2)) {
+    delta_fpr <- data$rate[i+1] - data$rate[i]
+    cor_totals <- data$cor_totals[i+1]
+    plot_data <- rbind(plot_data,
+                       data.frame(cor_totals = cor_totals, delta_fpr = delta_fpr))
   }
-
-  ggplot(plot_data, aes(x = fpr, y = tpr, color = method)) + # plot an ROC curve
-    geom_point(size = 2, alpha = 0.5) +
-    scale_color_manual(values = cpalette) +
-    xlim(c(0,1)) +
-    ylim(c(0,1)) +
-    xlab("FPR") +
-    ylab("TPR") +
-    facet_wrap(. ~ method) +
-    theme(legend.position = "none")
-  ggsave(file.path("output", "images", paste0("DE_p",p,"_all_models_partial.png")),
+  ggplot(plot_data, aes(x = cor_totals, y = delta_fpr)) +
+    geom_point(size = 1) +
+    geom_smooth(method = "lm", color = "#888888", alpha = 0.3)
+  ggsave(file.path("output", "images", "partial_total_performance.png"),
          units = "in",
-         height = 2.5,
-         width = 5)
+         height = 3,
+         width = 3)
+  
+  # ROC-like plots for NB GLM and NB GLM on partially faithful totals
+  # p <- 100
+  # data <- readRDS(file.path("output", paste0("simresults_p",p,"_simulated_all.rds")))
+  # 
+  # plot_data <- data %>%
+  #   select(delta_mean_v2, rate, rate_type, method, cor_totals) %>%
+  #   filter(method %in% c("baseline", "spike_in")) %>%
+  #   pivot_wider(names_from = rate_type, values_from = rate)
+  # plot_data$method <- factor(plot_data$method)
+  # levels(plot_data$method) <- c("NB GLM", "NB GLM (partial totals)")
+  # 
+  # binary_palette <- palette[c(1,5)]
+  # 
+  # ggplot(plot_data, aes(x = fpr, y = tpr, color = method)) +
+  #   geom_point(size = 2, alpha = 0.5) +
+  #   scale_color_manual(values = binary_palette) +
+  #   # xlim(c(0,1)) +
+  #   # ylim(c(0,1)) +
+  #   xlim(c(0,0.75)) +
+  #   ylim(c(0.25,1)) +
+  #   xlab("FPR") +
+  #   ylab("TPR") +
+  #   facet_wrap(. ~ method)
+  # ggsave(file.path("output", "images", paste0("DE_p",p,"_all_models_partial.png")),
+  #        units = "in",
+  #        height = 2.5,
+  #        width = 5)
 }
 
 # ------------------------------------------------------------------------------
@@ -83,8 +110,11 @@ if(partial_plot) {
 lfc_plot <- TRUE
 
 if(lfc_plot) {
-  data <- cbind(p = 100, readRDS(file.path("output", "simresults_p100_simulated_all.rds")))
-  for(p in c(1000, 5000, 15000)) {
+  unary_palette <- palette[2]
+  
+  pp <- c(1000, 5000, 15000)
+  data <- cbind(p = 1000, readRDS(file.path("output", paste0("simresults_p",pp[1],"_simulated_all.rds"))))
+  for(p in pp[2:length(pp)]) {
     data <- rbind(data, cbind(p = p, readRDS(file.path("output", paste0("simresults_p",p,"_simulated_all.rds")))))
   }
   data <- data %>%
@@ -92,28 +122,20 @@ if(lfc_plot) {
     filter(p > 100) %>%
     filter(rate_type == "fpr") %>%
     filter(delta_mean_v2 <= 10) %>%
-    select(p, delta_mean_v2, rate, method)
+    select(p, delta_mean_v2, rate, rate_type, method)
   data$p <- factor(data$p)
   
-  ggplot(data, aes(x = delta_mean_v2, y = rate, color = p)) + # plot an ROC curve
-    geom_point(size = 2, alpha = 0.5) +
-    geom_smooth(method = "loess") +
-    scale_color_manual(values = generate_highcontrast_palette(length(levels(data$p)))) #+
-    # facet_wrap(. ~ p)
+  ggplot(data, aes(x = log(delta_mean_v2), y = rate, color = rate_type)) + # plot an ROC curve
+    geom_point(size = 1, alpha = 0.25) +
+    geom_smooth(method = "loess", alpha = 0.5) +
+    scale_color_manual(values = unary_palette) +
+    theme(legend.position = "none") +
+    ylim(c(0,0.15))
+  ggsave(file.path("output", "images", "DESeq2_performance_curves.png"),
+         units = "in",
+         height = 3,
+         width = 3)
 }
-
-# ------------------------------------------------------------------------------
-#   DESeq2 performance (FPR) as a function of log fold difference in means
-# ------------------------------------------------------------------------------
-
-data <- plot_data %>%
-  filter(method %in% c("baseline", "spike_in")) %>%
-  filter(rate_type == "fpr") %>%
-  filter(cor_totals >= 0) %>%
-  select(cor_totals, rate, method)
-
-ggplot(data, aes(x = cor_totals, y = rate)) + # plot an ROC curve
-  geom_point(size = 2, alpha = 0.5)
 
 # ------------------------------------------------------------------------------
 #   FPR/TPR version
