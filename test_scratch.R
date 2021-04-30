e8647c4a86d7f75a513e2a7dbe50295186783634
kimberlyroche <kimberly.e.roche@gmail.com>
2021-04-29 20:21:33 -0400

    Added optional correlation of features to simulations

--- test_scratch.R
+++ test_scratch.R
@@ -2,47 +2,71 @@ library(codaDE)
 library(tidyverse)
 
 p <- 100
-n <- 10
+n <- 50
 
-iter <- 500
-asymmetry <- 0.8
-proportion_da <- 0.75
-spike_in <- TRUE
+iter <- 100
+asymmetry <- 1
+proportion_da <- 0.95
+spike_in <- FALSE
 
-mean_corr <- numeric(iter)
-lfc <- numeric(iter)
+A <- matrix(0.8, 100, 100)
+diag(A) <- 1
+B <- diag(100) # indep
+
+change_A <- c()
+change_B <- c()
 for(i in 1:iter) {
   # sim data
   build_simulated_reference(p = p, log_mean = 0, log_var = 2, log_noise_var = 1,
-                            base_correlation = 0.75, concentration = p + 100)
+                            base_correlation = A, concentration = p + 100)
   # ref_file <- file.path("data", paste0("DE_reference_simulated.rds"))
   # data_obj <- readRDS(ref_file)
-  
-  sim_data <- simulate_sequence_counts(n = n, p = p, ref_data = "simulated_bulk",
+
+  sim_data <- simulate_sequence_counts(n = n, p = p, ref_data = "simulated",
                                        asymmetry = asymmetry,
                                        proportion_da = proportion_da,
                                        spike_in = spike_in)
   
-  # eyeball what observed log-counts would look like
-  # ggplot(data.frame(x = log(round(data_obj$cond1) + 1)), aes(x = x)) +
-  #   geom_histogram()
+  # change_A <- c(change_A, sum(data_obj$log_perturbation))
+  # change_A <- c(change_A, sum(mean(data_obj$log_cond1) - mean(data_obj$log_cond2)))
+  # change_A <- c(change_A, sum(data_obj$cond2))
+  # change_A <- c(change_A, sum(sim_data$abundances[(n+1):(n*2),1:p]))
+  # change_A <- c(change_A, abs(abs(sum(sim_data$baseline_counts) - sum(sim_data$treatment_counts))))
+  change_A <- c(change_A, abs(sum(sim_data$abundances[1:n,1:p]) - sum(sim_data$abundances[(n+1):(n*2),1:p])))
   
-  # expr1 <- log(data_obj$cond1 + 1)
-  # expr2 <- log(data_obj$cond2 + 1)
-  # subset_idx <- sample(1:p, size = 100)
-  # plot_bipartite_graph(expr1[subset_idx], expr2[subset_idx], alpha = 0.5)
-
-  # K <- data_obj$correlation_matrix
-  # off_diag_corr <- K[upper.tri(K, diag = FALSE)]
-  # mean_corr[i] <- mean(off_diag_corr)
+  build_simulated_reference(p = p, log_mean = 0, log_var = 2, log_noise_var = 1,
+                            base_correlation = B, concentration = p + 100)
+  # ref_file <- file.path("data", paste0("DE_reference_simulated.rds"))
+  # data_obj <- readRDS(ref_file)
   
-  m1 <- mean(rowSums(sim_data$abundances[1:n,]))
-  m2 <- mean(rowSums(sim_data$abundances[(n+1):(n*2),]))
-  log_delta_mean <- log(max(c(m1, m2)) / min(c(m1, m2)))
-  lfc[i] <- log_delta_mean
+  sim_data <- simulate_sequence_counts(n = n, p = p, ref_data = "simulated",
+                                       asymmetry = asymmetry,
+                                       proportion_da = proportion_da,
+                                       spike_in = spike_in)
+  
+  # change_B <- c(change_B, sum(data_obj$log_perturbation))
+  # change_B <- c(change_B, sum(mean(data_obj$log_cond1) - mean(data_obj$log_cond2)))
+  # change_B <- c(change_B, sum(data_obj$cond2))
+  # change_B <- c(change_B, sum(sim_data$abundances[(n+1):(n*2),1:p]))
+  # change_B <- c(change_B, abs(abs(sum(sim_data$baseline_counts) - sum(sim_data$treatment_counts))))
+  change_B <- c(change_B, abs(sum(sim_data$abundances[1:n,1:p]) - sum(sim_data$abundances[(n+1):(n*2),1:p])))
 }
 
-# par(mfrow = c(1,2))
-# plot(mean_corr, lfc, xlim = c(0, 2))
-hist(lfc, xlim = c(0, 2))
+ggplot(data.frame(x = c(change_A, change_B),
+                  type = c(rep("A", iter), rep("B", iter))), aes(x = x, fill = type)) +
+  geom_density(alpha = 0.5) #+
+  #xlim(c(0, 20000))
+
+mean(change_A)
+mean(change_B)
+
+sd(change_A)
+sd(change_B)
+
+# In general the effect of correlation on the variance in total abundances is
+# pretty small.
+
+
+
+
 
