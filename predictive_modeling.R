8478918953722c9e55df1b5c8a4f52c1c0c8434f
kimberlyroche <kimberly.e.roche@gmail.com>
2021-09-30 07:01:01 -0400

    Added optional sample normalization to validation script

--- predictive_modeling.R
+++ predictive_modeling.R
@@ -8,12 +8,12 @@ library(optparse)
 DE_methods <- c("ALDEx2", "DESeq2", "scran")
 use_baseline <- "self"
 
-fit_predictive_model(DE_methods = DE_methods,
-                     use_baseline = use_baseline,
-                     output_weights = TRUE,
-                     exclude_partials = TRUE,
-                     exclude_independent = FALSE,
-                     fit_full = TRUE)
+# fit_predictive_model(DE_methods = DE_methods,
+#                      use_baseline = use_baseline,
+#                      output_weights = TRUE,
+#                      exclude_partials = TRUE,
+#                      exclude_independent = FALSE,
+#                      fit_full = TRUE)
 
 for(use_result_type in c("TPR", "FPR")) {
   # Evaluate TPR model
@@ -21,10 +21,20 @@ for(use_result_type in c("TPR", "FPR")) {
                                "predictive_fits",
                                paste0(use_baseline, "_", use_result_type, ".rds")))
 
-  test_data <- cbind(fit_obj$test_features, response = fit_obj$test_response)
-  
+  test_data <- cbind(fit_obj$test_features, response = fit_obj$test_response)  
   prediction <- predict(fit_obj$result, newdata = test_data) # predict.all = TRUE
 
-  cat(paste0("R^2 (",use_result_type,"): ",
+  # Overall
+  cat(paste0("Overall R^2 (",use_result_type,"): ",
              round(cor(fit_obj$test_response, prediction)^2, 3), "\n"))
+
+  for(p in sort(unique(test_data$p))) {
+    # Per-P
+    idx <- which(test_data$P == p)
+    prediction <- predict(fit_obj$result, newdata = test_data[idx,]) # predict.all = TRUE
+
+    cat(paste0("P=",p," R^2 (",use_result_type,"): ",
+               round(cor(fit_obj$test_response[idx], prediction)^2, 3), "\n"))
+  }
+
 }
